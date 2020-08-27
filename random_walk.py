ee6327cb08e16375a3e58e02aa6a8708888ad8c0
HayatoTanoue <hayatotanoue7321@gmail.com>
2020-08-28 02:02:41 +0900

    強化量固定ランダムウォークの修正

--- random_walk.py
+++ random_walk.py
@@ -1,3 +1,4 @@
+import random
 import numpy as np
 import networkx as nx
 
@@ -64,37 +65,6 @@ def select_by_reverse_degree(G, now):
     selected =  np.random.choice(list(G.neighbors(now)), 
                                  size=1, 
                                  p=prob)
-    selected = selected[0]
-    
-    return selected
-
-def select_by_weight(G, now, matrix):
-    """
-    エッジの重みに応じた選択確率でノードを選択する
-
-    Parameters
-    ----------
-    G : networkx graph
-        
-    now : int
-        walkerの現状の位置(ノード番号)
-        
-    matrix : numpy matrix
-        networkの遷移行列
-
-    Returns
-    -------
-    selected : int
-        選択されたノード番号
-
-    """
-    
-    #重み
-    prob = [weight / sum(matrix[now]) for weight in matrix[now] ]
-    
-    selected = np.random.choice(list( np.arange(nx.number_of_nodes(G) ) ),
-                                size=1,
-                                p=prob)
     selected = selected[0]
     
     return selected
@@ -230,26 +200,32 @@ def fixed_rainforce_random_walk(G, walk_length, start_position, add_weight):
     now = start_position
     length = 0
     
-    #networkの遷移行列の作成
-    matrix = nx.to_numpy_array(G)
-    
+    now = start_position
+    length = 0
+
     #訪問ノードリスト
     walk = list()
+
+    #隣接リストの作成
+    adj_list = nx.to_dict_of_lists(G)
+    #重みリストの作成
+    weight_list = {}
+    for l in range(nx.number_of_nodes(G)):
+        weight_list.setdefault(l, [1 for i in adj_list[l]])
     
     while length < walk_length:
         
-        #エッジの重みに応じて選択確率は変化, 選択されたエッジを固定分強化
-        selected = select_by_weight(G, now, matrix)
+        #エッジの重みに応じて選択確率は変化, 選択されたエッジを次数分強化
+        selected = random.choices(adj_list[now], k=1, weights=weight_list[now])[0]
         
-        #選択された(通過した)エッジに重みを追加する
-        matrix[now, selected] += add_weight
-        matrix[selected, now] += add_weight
+        #選択された(通過した)エッジに重みを追加する(強化量固定)
+        weight_list[ now ][ adj_list[now].index(selected) ] += add_weight
+        weight_list[ selected ][ adj_list[selected].index(now) ] += add_weight
         
         walk.append(selected)
         
         now = selected
         length += 1
-    
     return walk
 
 def degree_reinforce_random_walk(G, walk_length, start_position):
